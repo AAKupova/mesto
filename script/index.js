390e00612de805395d3d64cb8c0dd5f66dd47d76
Kupova Anastasia <aa.kupova@yandex.ru>
2021-06-11 10:36:13 +0300

    fix: исправление js

--- script/index.js
+++ script/index.js
@@ -2,40 +2,28 @@ const root = document.querySelector(".root");
 const editButton = root.querySelector(".profil__edit");
 const closeButton = root.querySelector(".popup__close");
 const formElement = root.querySelector(".form");
-const listElement = root.querySelector(".cards__list");
 const popup = root.querySelector(".popup");
-const submitButton = root.querySelector(".form__submit-button");
 const nameInput = root.querySelector(".form__input_type_name");
 const jobInput = root.querySelector(".form__input_type_description");
 const profilName = root.querySelector(".profil__name");
 const profilText = root.querySelector(".profil__text");
 
-function openPopup() {
-  popup.classList.remove("popup_hidden");
-}
+function hiddenPopup() {
+  nameInput.value = profilName.innerText;
+  jobInput.value = profilText.innerText;
 
-function closePopup({ target }) {
-  if (target === popup || target === closeButton || target === submitButton) {
-    popup.classList.add("popup_hidden");
-  }
+  popup.classList.toggle("popup_hidden");
 }
 
 function formSubmitHandler(event) {
   event.preventDefault();
 
-  profilName.textContent = nameInput.value;
-  profilText.textContent = jobInput.value;
+  profilName.innerText = nameInput.value;
+  profilText.innerText = jobInput.value;
 
-  closePopup(event);
+  hiddenPopup();
 }
 
-function like({ target }) {
-  if (target.classList.contains("cards__icon")) {
-    target.classList.toggle("cards__icon_activ");
-  }
-}
-
-editButton.addEventListener("click", openPopup);
-popup.addEventListener("click", closePopup);
+editButton.addEventListener("click", hiddenPopup);
+closeButton.addEventListener("click", hiddenPopup);
 formElement.addEventListener("submit", formSubmitHandler);
-listElement.addEventListener("click", like);
